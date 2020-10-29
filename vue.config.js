ce192588f5766f286ea7149b7b78e6e8c52a0eb5
Roc Wang <roc@kiwiberry.nz>
2020-10-29 13:59:18 +1300

    fix: fix the "as" attribute of preload tags

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = {
     webpackConfig.plugin("preload").tap(args => {
       const [options] = args;
       options.include = "allAssets";
+      options.as = entry => {
+        if (/\.css$/.test(entry)) return "style";
+        if (/\.woff2$/.test(entry)) return "font";
+        if (/\.(png|svg)$/.test(entry)) return "image";
+
+        return "script";
+      };
 
       return args;
     });
