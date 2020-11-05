c763b82dccf2ce6f12accbfef1da2f0669a67404
Roc Wang <roc@kiwiberry.nz>
2020-11-05 14:11:32 +1300

    feat: add the description meta tag

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -3,6 +3,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <meta name="theme-color" content="#1e1e20" />
+    <meta name="description" content="名媛”群邀请函示例, by Roc Wang" />
     <title>上海名媛3群邀请函</title>
     <link rel="icon" :href="favicon" />
     <link rel="author" href="https://space.bilibili.com/700294461" />
